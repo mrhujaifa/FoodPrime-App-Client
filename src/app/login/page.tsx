1dda45191ed9b9b6f5097080d74462f3aa113c64
mrhujaifa <mdhujaifaislamshanto@gmail.com>
2026-02-11 20:42:19 +0600

    style(login, signUp): responsive background img

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,10 +6,15 @@ export default function LoginPage() {
   return (
     <div className="relative flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       {/* Background Image Layer */}
-      <div className="absolute inset-0 -z-10 h-full w-full overflow-hidden">
-        <Image src={authBg} alt="Background" className="w-full" />
+      <div className="absolute inset-0 -z-10 h-full w-full">
+        <Image
+          src={authBg}
+          alt="Background"
+          fill
+          priority
+          className="object-cover"
+        />
       </div>
-
       {/* Form Container */}
       <div className="relative z-10 flex w-full max-w-sm flex-col gap-6">
         <LoginForm />
